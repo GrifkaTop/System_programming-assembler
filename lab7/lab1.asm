4c78ee151606e09ec572388fe17a204062d4128c
GrifkaTop <grifkatop@gmail.com>
2026-02-24 11:59:00 +0300

    7

--- lab7/lab1.asm
+++ lab7/lab1.asm
@@ -7,15 +7,19 @@ include 'lib.asm'
 section '.data' writeable
     ask_text    db 10, "Введите команду: ", 0
     fail_text   db "Ошибка", 10, 0
-    cmd_line    rb 256
-    cmd_args    dq 0, 0 ; Массив аргументов для execve
-    env         dq 0    ; Переменные окружения
+    cmd_line    rb 256 ; буфер команлы
+    
+    ; Массив аргументов для execve: [адрс_cmd, адр_арг1, ..., 0]
+    align 8
+    cmd_args    dq 0, 0, 0, 0, 0, 0, 0, 0 ; Резервируем место под 8 аргументов
+    
+    env         dq 0    ; указатель на окружение
 
 ; ----------------------------------Код-------------------------------
 section '.text' executable
 _start:
     ; Получаем окружение из стека
-    pop rcx
+    pop rcx 
     lea rdi, [rsp + rcx*8 + 8] 
     mov [env], rdi
 
@@ -24,40 +28,83 @@ main_loop:
     call print_str
 
     mov rsi, cmd_line
-    call input_keyboard ; Считали команду в cmd_line
+    call input_keyboard ; Считали команду в cmd_line 
 
-    cmp byte [cmd_line], 0 ; Если пользователь просто нажал Enter, то повторяем запрос
-    je main_loop ; Если команда не пустая, то пытаемся выполнить ее
+    cmp byte [cmd_line], 0  ; Enter ?
+    je main_loop 
 
-    ; Подготовка аргументов (argv[0] = путь к файлу, argv[1] = NULL)
-    mov [cmd_args], cmd_line
+; --- РАЗБОР СТРОКИ НА АРГУМЕНТЫ ---
+    lea rsi, [cmd_line] ; Указатель на начало строки
+    lea rdi, [cmd_args] ; Указатель на массив аргументов
+    mov qword [rdi], rsi    ; Первый аргумент — всегда начало строки (имя файла)
+    add rdi, 8 ;
 
-    mov rax, 57         ; sys_fork - создает новый процесс, который является копией текущего процесса. Возвращает 0 в дочернем процессе, PID ребенка в родительском процессе, и -1 при ошибке
+parse_loop:
+    mov al, [rsi] 
+    cmp al, 0               ; Конец строки?
+    je finish_parse
+    cmp al, ' '             ; Пробел?
+    jne next_char
+
+    ; Если нашли пробел:
+    mov byte [rsi], 0       ; Заменяем пробел на конец строки для предыдущего аргумента
+    inc rsi
+    ; Пропускаем лишние пробелы, если они есть
+skip_spaces:
+    cmp byte [rsi], ' '
+    jne check_end
+    inc rsi
+    jmp skip_spaces
+check_end:
+    cmp byte [rsi], 0 ; Конец строки после пробелов ?
+    je finish_parse     
+    
+    mov [rdi], rsi          ; Сохраняем адрес начала следующего аргумента
+    add rdi, 8 
+    jmp parse_loop
+
+next_char:
+    inc rsi
+    jmp parse_loop
+
+finish_parse:
+    mov qword [rdi], 0      ; Последний элемент массива ДОЛЖЕН быть NULL
+
+; --------- Конец разбора строки на аргументы ---------
+; -------------- ФОРК И ВЫПОЛНЕНИЕ --------------
+    mov rax, 57         ; sys_fork
     syscall 
 
     test rax, rax
-    js  main_loop       ; Ошибка fork - просто повторяем запрос
-    jz  do_exec         ; Дочерний процесс
+    js  main_loop       
+    jz  do_exec         
     
     ; Родительский процесс: ждем завершения
-    mov rdi, rax        ; PID ребенка, PID - это возвращаемое значение fork
-    mov rax, 61         ; sys_wait4 - ждем завершения дочернего процесса
+    mov rdi, rax        
+    mov rax, 61         ; sys_wait4
     xor rsi, rsi
     xor rdx, rdx
     xor r10, r10
     syscall
+    
+    ; Очистка массива аргументов перед следующим вводом
+    lea rdi, [cmd_args]
+    mov rcx, 8
+    xor rax, rax
+    rep stosq
+    
     jmp main_loop
 
 ; Выполнение команды в дочернем процессе
 do_exec:
     mov rax, 59         ; sys_execve
-    mov rdi, cmd_line ; Путь к исполняемому файлу
-    lea rsi, [cmd_args] ; argv
-    mov rdx, [env]   ; Передаем окружение
+    mov rdi, [cmd_args] ; Путь к исполняемому файлу (первый токен)
+    mov rsi, cmd_args   ; Массив всех аргументов
+    mov rdx, [env]      ; Окружение
     syscall
 
-    mov rsi, fail_text ; Если execve возвращает, значит произошла ошибка
-    call print_str ; Печатаем сообщение об ошибке
-    mov rax, 60         ; exit
-    mov rdi, 1 ; код ошибки
+    mov rsi, fail_text 
+    call print_str 
+    mov rax, 60         
+    mov rdi, 1 
     syscall
--- lab7/lab2.asm
+++ lab7/lab2.asm
@@ -1,109 +1,284 @@
 format ELF64
 public _start
 
+; --- Константы ---
+SYS_WRITE   equ 1
+SYS_MMAP    equ 9
+SYS_CLONE   equ 56
+SYS_EXIT    equ 60
+SYS_WAIT4   equ 61
 
-include 'lib.asm'
-include 'sort.inc'
-include 'task_mode.inc'
-include 'task_fifth.inc'
-include 'task_quantile.inc'
-include 'task_primes.inc'
+PROT_RW     equ 0x3
+MAP_ANON    equ 0x22
+CLONE_VM_SH equ 0x111 
 
-; --- Настройка размера массива ---
-ELEMENT_COUNT = 10
+; ПАРАМЕТРЫ ВАРИАНТА
+ARRAY_SIZE  equ 669       ; Ваш размер массива
+STACK_SIZE  equ 65536     ; Размер стека для каждого потока в куче 64к
 
-;----------------------------------Данные и константы-------------------------------
+; --- Сегменты данных и кода ---
 section '.data' writeable
-    count       dq ELEMENT_COUNT 
-    msg_m       db "1. Мода: ", 0 
-    msg_f       db "2. 5-й элемент после сортировки: ", 0
-    msg_q       db "3. 0.75 квантиль: ", 0
-    msg_p       db "4. Количество простых чисел: ", 0
-    nl          db 10, 0 
+    heap_ptr dq 0
+    msg1 db "1. Самая частая цифра: ", 0
+    msg2 db 10, "2. 5-й элемент: ", 0
+    msg3 db 10, "3. 0.75 квантиль: ", 0
+    msg4 db 10, "4. Кол-во простых чисел: ", 0
 
-section '.bss' writeable
-    ; Резервируем память под ELEMENT_COUNT 64-битных чисел
-    array       rq ELEMENT_COUNT 
-
-;----------------------------------Код-------------------------------
 section '.text' executable
 _start:
-    ; 0. Генерируем случайные числа (системный вызов getrandom)
-    mov rax, 318        ; sys_getrandom
-    mov rdi, array
-    mov rsi, [count]
-    shl rsi, 3          ; count * 8 байт
-    xor rdx, rdx        ; флаги = 0
+    ; 1. Выделение кучи под массив (669 элементов * 8 байт)
+    mov rax, SYS_MMAP
+    xor rdi, rdi
+    mov rsi, ARRAY_SIZE * 8
+    mov rdx, PROT_RW
+    mov r10, MAP_ANON
     syscall
+    mov [heap_ptr], rax
 
-    ; 1. Приведение к натуральным числам < 1 000 000
-    mov rcx, [count]
-    test rcx, rcx
-    jz .skip_init       ; Если массив пустой, выходим
-    mov rsi, array
-.make_natural:
-    mov rax, [rsi]
-    test rax, rax      
-    jns .is_positive   
-    neg rax            ; Модуль числа
-.is_positive:
-    xor rdx, rdx
-    mov rbx, 1000000   
-    div rbx            ; Ограничиваем диапазон
-    mov [rsi], rdx      
-    add rsi, 8
-    loop .make_natural
+    ; Заполнение случайными числами (0-999)
+    mov rcx, ARRAY_SIZE
+    mov rdi, [heap_ptr]
+.gen:
+    rdrand rax      ; случайно числов в rax
+    jnc .gen        ; если не сработал
+    xor rdx, rdx    ; Очистим rdx для деления
+    mov rbx, 1000   ; /1000
+    div rbx         ; rax = rax / 1000, rdx = rax % 1000
+    mov [rdi], rdx  ; Сохраняем число в массив
+    add rdi, 8
+    loop .gen
 
-.skip_init:
-
-    ; 2. Создание 4-х дочерних процессов
-    mov rcx, 4
-.fork_loop: 
-    push rcx            ; Сохраняем номер задачи (4, 3, 2, 1)
-    mov rax, 57         ; sys_fork
-    syscall
-    
-    test rax, rax 
-    jz .child_work      ; Если RAX=0, это ребенок
-    
-    pop rcx             ; В родителе восстанавливаем счетчик
-    loop .fork_loop     ; Цикл до 0
-
-    ; 3. Родительский процесс: ждет всех детей
-    mov rcx, 4
-.wait_all:
+    ; --- СОРТИРОВКА  ---
+    mov rcx, ARRAY_SIZE
+    dec rcx         ; Для корректной работы j+1
+.out:
     push rcx
-    mov rax, 61         ; sys_wait4
-    mov rdi, -1         ; Ждать любого ребенка
+    xor rbx, rbx
+    mov rsi, [heap_ptr]
+.in:
+    mov rax, [rsi + rbx*8]
+    mov rdx, [rsi + rbx*8 + 8]
+    cmp rax, rdx
+    jle .no_swp
+    mov [rsi + rbx*8], rdx      ; Сохраняем меньшее число
+    mov [rsi + rbx*8 + 8], rax  ; Сохраняем большее число
+.no_swp:
+    inc rbx
+    cmp rbx, rcx
+    jl .in
+    pop rcx
+    loop .out
+
+    ; 2. Запуск потоков
+    mov rsi, task_frequent_digit
+    call spawn_thread
+    mov rsi, task_fifth_after_min
+    call spawn_thread
+    mov rsi, task_quantile
+    call spawn_thread
+    mov rsi, task_count_primes
+    call spawn_thread
+
+    ; 3. Родитель ждет 4 завершения
+    mov rcx, 4
+.wait:
+    push rcx
+    mov rax, SYS_WAIT4
+    mov rdi, -1 
     xor rsi, rsi 
-    xor rdx, rdx 
-    xor r10, r10
-    syscall 
-    pop rcx 
-    loop .wait_all
-    
-    mov rax, 60         ; Выход родителя
-    xor rdi, rdi 
-    syscall 
-
-
-; --- Логика дочерних процессов ---
-.child_work:
-    pop rbx             ; Достаем ID задачи (номер из push rcx)
-    cmp rbx, 4 
-    je do_mode  ; Задача 1: Мода
-    cmp rbx, 3 
-    je do_fifth ; Задача 2: 5-й элемент после сортировки
-    cmp rbx, 2  
-    je do_quantile ; Задача 3: 0.75 квантиль
-    cmp rbx, 1 
-    je do_primes  ; Задача 4: Количество простых чисел
-    jmp exit_child 
-    
-; --- Точка завершения любого ребенка ---
-exit_child: 
-    mov rsi, nl
-    call print_str      
-    mov rax, 60         ; sys_exit
-    xor rdi, rdi 
+    xor rdx, rdx
+    xor r10, r10 
     syscall
+    pop rcx
+    loop .wait
+
+    ; Завершение родителя
+    mov rax, SYS_EXIT
+    xor rdi, rdi
+    syscall
+
+; --- Хелпер создания потока ---
+spawn_thread: 
+    push rsi
+    mov rax, SYS_MMAP    ; Стек потока тоже выделяем в куче
+    xor rdi, rdi
+    mov rsi, STACK_SIZE
+    mov rdx, PROT_RW
+    mov r10, MAP_ANON
+    syscall
+    lea rsi, [rax + STACK_SIZE]
+    pop r8
+    mov rax, SYS_CLONE
+    mov rdi, CLONE_VM_SH
+    syscall
+    test rax, rax
+    jz .child
+    ret
+.child:
+    jmp r8
+
+
+; ------------------------ ЗАДАЧИ ДЛЯ ПОТОКОВ --------------------------
+; 1. Часто встречающаяся цифра
+task_frequent_digit:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 80         ; Локальные счетчики на стеке потока (10 цифр * 8 байт)
+    
+    ; Обнуляем счетчики
+    mov rdi, rsp
+    mov rcx, 10
+    xor rax, rax
+    rep stosq           ; Заполняем нулями область [rsp..rsp+79] 
+
+    mov rsi, [heap_ptr]
+    mov rcx, ARRAY_SIZE
+.l1: 
+    mov rax, [rsi]      ; Берем число из массива в куче
+    add rsi, 8
+    push rcx            ; Сохраняем счетчик цикла
+.l2: 
+    xor rdx, rdx
+    mov rbx, 10
+    div rbx             ; Выделяем цифру в rdx
+    inc qword [rbp - 80 + rdx*8] ; Увеличиваем счетчик этой цифры
+    test rax, rax
+    jnz .l2             ; Пока число не кончится
+    pop rcx             ; Возвращаем счетчик цикла
+    loop .l1
+
+    ; Ищем максимум среди 10 счетчиков
+    xor rbx, rbx        ; Индекс (цифра)
+    xor rdx, rdx        ; Максимальное значение
+    xor rcx, rcx
+.m: 
+    mov rax, [rbp - 80 + rcx*8]
+    cmp rax, rdx
+    jle .n
+    mov rdx, rax
+    mov rbx, rcx        ; rbx = самая частая цифра
+.n: 
+    inc rcx
+    cmp rcx, 10
+    jl .m
+    
+    push rbx            
+    mov rdi, msg1
+    call print_str
+    pop rax            
+    call print_int
+    
+    leave               
+    jmp thread_exit
+
+; 2. Пятое после минимального 
+task_fifth_after_min:
+    mov rsi, [heap_ptr]
+    mov rax, [rsi + 5*8]
+    
+    push rax          
+    mov rdi, msg2
+    call print_str
+    pop rax
+
+    call print_int
+    jmp thread_exit
+
+; 3. 0.75 квантиль 
+task_quantile:
+    mov rsi, [heap_ptr]
+    ; 669 * 3 / 4 = 501
+    mov rax, [rsi + 501*8]
+    
+    push rax           
+    mov rdi, msg3
+    call print_str
+    pop rax
+    
+    call print_int
+    jmp thread_exit
+
+; 4. Количество простых
+task_count_primes:
+    mov rsi, [heap_ptr]
+    mov rcx, ARRAY_SIZE
+    xor r12, r12
+.p: mov rax, [rsi]
+    push rcx
+    call check_prime
+    pop rcx
+    add r12, rax
+    add rsi, 8
+    loop .p
+    
+    mov rax, r12 
+
+    push rax
+    mov rdi, msg4
+    call print_str
+    pop rax
+
+    call print_int
+    jmp thread_exit
+
+thread_exit:
+    mov rax, SYS_EXIT
+    xor rdi, rdi
+    syscall
+
+; --- Проверка простоты ---
+check_prime:
+    cmp rax, 2
+    jl .no
+    je .yes
+    mov rbx, 2
+.c: xor rdx, rdx
+    mov r8, rax
+    div rbx
+    test rdx, rdx
+    jz .no
+    inc rbx
+    mov rax, rbx
+    mul rbx
+    cmp rax, r8
+    mov rax, r8
+    jle .c
+.yes: mov rax, 1
+    ret
+.no: xor rax, rax
+    ret
+
+; --- Функции вывода ---
+print_str:
+    mov rsi, rdi
+    xor rdx, rdx
+.l: cmp byte [rsi+rdx], 0
+    je .o
+    inc rdx
+    jmp .l
+.o: mov rax, SYS_WRITE
+    mov rdi, 1
+    syscall
+    ret
+
+print_int:
+    sub rsp, 40
+    mov rbx, 10
+    lea rdi, [rsp + 38]
+    xor rcx, rcx
+.c: xor rdx, rdx
+    div rbx
+    add dl, '0'
+    mov [rdi], dl
+    dec rdi
+    inc rcx
+    test rax, rax
+    jnz .c
+    inc rdi
+    mov rsi, rdi
+    mov rdx, rcx
+    mov rax, SYS_WRITE
+    mov rdi, 1
+    syscall
+    add rsp, 40
+    ret
